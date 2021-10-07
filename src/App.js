f329ed827a80aed857298eb72042f8b348fff2b1
Muhammad Zain <Zainsaleem456@gmail.com>
2021-10-07 19:14:59 +0500

    React task 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Swal from "sweetalert2";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input,
+  Button,
+  Table,
+  Col,
+  Row,
+  Container,
+} from "reactstrap";
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  
+  const [name, setName] = useState("");
+  const [pass, setPass] = useState("");
+  let history = useHistory();
+  const loginUser = () => {
+      let username = "admin@domain.com"
+      let userPass = "admin";
+
+      if(name == username && pass == userPass){
+
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'User Logged In',
+          showConfirmButton: false,
+          timer: 1500
+        })
+        history.push("/login");
+
+
+      }
+      else{
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'incorrect Username or Password!',
+          footer: '<a href="">Why do I have this issue?</a>'
+        })
+      }
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col md={4}>
+            <InputGroup>
+             
+              <Input
+                onChange={(event) => setName(event.target.value)}
+                placeholder="username"
+              />
+            </InputGroup>
+          </Col>
+
+          <Row>
+            <Col md={4}>
+              <InputGroup>
+               
+                <Input type="password"
+                  onChange={(event) => setPass(event.target.value)}
+                  placeholder="Password"
+                />
+              </InputGroup>
+            </Col>
+          </Row>
+
+          <Col>
+          <Button color="primary" onClick={loginUser}>Login</Button>{' '}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/pages/login.jsx
@@ -0,0 +1,275 @@
+// import logo from "./logo.svg";
+import ".././App.css";
+import Swal from "sweetalert2";
+import {
+  FaBeer,
+  FaAddressCard,
+  FaUserPlus,
+  FaPlus,
+  FaTrashAlt,
+  FaRegEdit,
+} from "react-icons/fa";
+import {
+  FloatingMenu,
+  MainButton,
+  ChildButton,
+} from "react-floating-button-menu";
+//   import MdAdd from '@material-ui/icons/add';
+//   import MdClose from '@material-ui/icons/clear';
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input,
+  Table,
+  Col,
+  Row,
+  Button,
+  Container,
+  Modal,
+  ModalHeader,
+  Label,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+} from "reactstrap";
+// import { Container, Button, Link } from 'react-floating-action-button'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+
+const Login = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [salary, setSalary] = useState("");
+  const [date, setDate] = useState("");
+  const [data, setData] = useState([]);
+  const [isIndex, setIsIndex] = useState("")
+  let [isOpen, setOpen] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [isUpdate, setIsUpdate] = useState(false);
+  
+  const toggle = () => {
+    setModal(!modal);
+    setIsUpdate(false);
+    setFirstName("");
+    setLastName("")
+    setEmail("")
+    setSalary("")
+    setDate("")
+  };
+
+  const closeBtn = (
+    <button className="close" onClick={toggle}>
+      &times;
+    </button>
+  );
+
+  const updateEmp = () => {
+    console.log(firstName);
+    
+    // let obj = {
+      //   firstName,
+      //   lastName,
+      //   email,
+      //   salary,
+      //   date,
+      // }
+      let dupData = [...data]
+      
+      dupData[isIndex].fName = firstName
+      dupData[isIndex].lName = lastName
+      dupData[isIndex].Email = email
+      dupData[isIndex].salary = salary
+      dupData[isIndex].date = date
+      
+      
+      // let updateData = setData([...data[isIndex], obj])
+      // console.log(updateData);
+      setData(dupData)
+      
+      setModal(false);
+      // console.log(obj);
+  }
+
+  const editEmp = (edit) => {
+    setModal(true);
+    setIsUpdate(true);
+    setIsIndex(edit);
+    setFirstName(data[edit].fName);
+    setLastName(data[edit].lName)
+    setEmail(data[edit].Email)
+    setSalary(data[edit].salary)
+    setDate(data[edit].date)
+    
+    
+  };
+  // console.log(isIndex);
+
+  const dltEmp = (e) => {
+    // console.log(e);
+    data.map((per, index) => {
+      // console.log(index);
+      if (e == index) {
+        console.log("Matched !" + index + " &  " + e);
+        let updatedData = data.splice(e, 1);
+        setData([...data]);
+      }
+    });
+  };
+
+  const addEmp = () => {
+    let obj = {
+      fName: firstName,
+      lName: lastName,
+      Email: email,
+      salary: salary,
+      date: date,
+    };
+
+    console.log(firstName);
+    setData([...data, obj]);
+    setModal(false);
+  };
+  // console.log(data);
+  let count = 0;
+  return (
+    <div className="App">
+      <Container>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Salary</th>
+              <th>Joining Date</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((per, index) => (
+              <tr key={index}>
+                <td>{++count}</td>
+                <td>{per.fName}</td>
+                <td>{per.lName}</td>
+                <td>{per.Email}</td>
+                <td>{per.salary}</td>
+                <td>{per.date}</td>
+                <td>
+                  {
+                    <Button
+                      className="ml-3"
+                      color="outline-danger"
+                      onClick={() => dltEmp(index)}
+                    >
+                      <FaTrashAlt />
+                    </Button>
+                  }
+                  {
+                    <Button
+                      className="ml-4"
+                      color="outline-success"
+                      onClick={() => editEmp(index)}
+                    >
+                      <FaRegEdit />
+                    </Button>
+                  }
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <Button className="btn-add-user" onClick={toggle}>
+          <FaUserPlus />
+        </Button>{" "}
+      </Container>
+
+      <div>
+        <Modal isOpen={modal} toggle={toggle}>
+          <ModalHeader toggle={toggle} close={closeBtn}>
+            Modal title
+          </ModalHeader>
+          <ModalBody>
+            <Container>
+              <Row>
+                <Col md={6}>
+                  <InputGroup>
+                    <Input
+                      onChange={(event) => setFirstName(event.target.value)}
+                      value={firstName}
+                      placeholder="FirstName"
+                    />
+                  </InputGroup>
+                </Col>
+
+                <Col md={6}>
+                  <InputGroup>
+                    <Input
+                      onChange={(event) => setLastName(event.target.value)}
+                      placeholder="LastName"
+                      value={lastName}
+                    />
+                  </InputGroup>
+                </Col>
+              </Row>
+
+              <Row className="py-4">
+                <Col>
+                  {" "}
+                  <InputGroup>
+                    <InputGroupText>@</InputGroupText>
+
+                    <Input
+                      onChange={(event) => setEmail(event.target.value)}
+                      placeholder="Email"
+                      type="email"
+                      value={email}
+                    />
+                  </InputGroup>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col md={6}>
+                  <Input
+                    type="number"
+                    placeholder="Salary"
+                    min={1000}
+                    onChange={(event) => setSalary(event.target.value)}
+                    value={salary}
+                  />
+                </Col>
+
+                <Col md={6}>
+                  <Input
+                    type="date"
+                    onChange={(event) => setDate(event.target.value)}
+                    value={date}
+                  />
+                </Col>
+              </Row>
+            </Container>
+          </ModalBody>
+          <ModalFooter>
+            {!isUpdate ? (
+              <Button color="primary" onClick={addEmp}>
+                Add Employee <FaPlus />
+              </Button>
+            ) : (
+              <Button color="primary" onClick={updateEmp}>
+                Update <FaPlus />
+              </Button>
+            )}
+          </ModalFooter>
+        </Modal>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
